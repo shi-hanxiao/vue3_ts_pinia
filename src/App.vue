402a9013d16ff76cfd2dc334240b6789b1d3eb53
15090000339 <1351211775@qq.com>
2023-06-11 15:02:46 +0800

    feat: init

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { Delete } from "@element-plus/icons-vue";
+</script>
 
 <template>
   <div>
     <h1>APP</h1>
+    <el-button type="primary" :icon="Delete">删除</el-button>
+    <svg>
+      <use xlink:href="#icon-item" fill="red"></use>
+    </svg>
+    <Pagination />
   </div>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,26 @@
 import { createApp } from "vue";
-import App from "./App.vue";
+import App from "@/App.vue";
+//引入模板的全局的样式
+import "@/styles/index.scss";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import zhCn from "element-plus/dist/locale/zh-cn.mjs";
+// svg插件需要配置代码
+import "virtual:svg-icons-register";
+// 引入自定义插件对象：注册整个项目全局组件
+import globalComponent from "@/components/index";
+import request from "@/utils/request";
+const app = createApp(App);
+app.use(ElementPlus, {
+  locale: zhCn,
+});
+app.use(globalComponent);
+app.mount("#app");
 
-createApp(App).mount("#app");
+request({
+  url: "/user/login",
+  method: "post",
+  data: { username: "admin", password: "111111" },
+});
